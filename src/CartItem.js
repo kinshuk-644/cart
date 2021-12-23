6d302ca77b810a36aa6c36421c97ade76b8b9bb6
Kinshuk <kinshuktaya644@gmail.com>
2021-12-23 16:06:23 +0530

    Add buttons and implement state in React

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
     render() {
+        const {price, title, qty} = this.state;
+        
         return (
             <div className='cart-item'>
                 <div className='left-block'>
@@ -9,12 +21,15 @@ class CartItem extends React.Component {
                 </div>
 
                 <div className='right-block'>
-                    <div style={ { fontSize: 25 } }>Phone</div>
-                    <div style={ { color: '#777' } }>Rs 999</div>
-                    <div style={ { color: '#777' } }>Qty: 1</div>
+                    <div style={ { fontSize: 25 } }>{title}</div>
+                    <div style={ { color: '#777' } }>Rs {price}</div>
+                    <div style={ { color: '#777' } }>Qty: {qty}</div>
 
                     <div className='cart-item-actions'>
                         {/* Buttons */}
+                        <img alt='increase' className='action-icons' src='https://as1.ftcdn.net/v2/jpg/02/51/03/82/1000_F_251038282_CLb3d8tk99bGoU9ILEYS8vY215fgRmGT.jpg' />
+                        <img alt='decrease' className='action-icons' src='https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg' />
+                        <img alt='delete' className='action-icons' src='https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1640255704~hmac=7280aae68b1aba11bcbb853e0a8a823c' />
                     </div>
                 </div>
             </div>
